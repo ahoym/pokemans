f9c5253e902e419f5d948928e1fbc12cb7dc5f33
Malcolm Ahoy <malcolmahoy@gmail.com>
2018-07-31 17:27:46 -0700

    Extract response normalization to utils and import
    This was common logic, and sets the default normalization. Each request
    can still override if needed.
    
    Lets the normalization logic only need to be tested once

--- src/utils/makeRequest.js
+++ src/utils/makeRequest.js
@@ -10,9 +10,20 @@ here may be:
 These are all things that are handy for all requests, and not just in the
 context of async actions in redux.
 */
+import camelize from 'camelize';
 
 export default function request({ endpoint }) {
   return fetch(endpoint).then(response => response.json());
+}
+
+/**
+ * Normalizes responses from the endpoint into a format the frontend expects
+ *
+ * @param {Object} response body from the XhrResponse
+ * @returns {Object} of the response object with camelized keys
+ */
+export function successNormalizer(response) {
+  return response => camelize(response);
 }
 
 // Simple closure around request, for redesert-api-thunk
